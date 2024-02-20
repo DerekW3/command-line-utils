781d229a216a4cde2f5610d2eae8df9853f2ed57
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-02-20 16:40:53 -0600

    Build: Accept CLI arguments

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -10,4 +10,32 @@ pub struct Config {
     bytes: Option<usize>,
 }
 
-
+pub fn get_args() -> MyResult<Config> {
+    let mut matches = Command::new("headr")
+        .version("0.1.0")
+        .author("Derek Warner <derekw3@illinois.edu>")
+        .about("A rusty head")
+        .arg(
+            Arg::new("files")
+                .help("input files")
+                .default_value("-")
+                .num_args(1..),
+        )
+        .arg(
+            Arg::new("number_lines")
+                .short('n')
+                .long("lines")
+                .help("number of lines to print")
+                .num_args(1)
+                .default_value("10")
+                .conflicts_with("bytes"),
+        )
+        .arg(
+            Arg::new("bytes")
+                .short('c')
+                .long("bytes")
+                .help("number of bites to print")
+                .num_args(1)
+                .default_value("10"),
+        );
+}
---
+++ headr/target/debug/deps/ppv_lite86-9bf332c1fd2b4dcb.d
@@ -0,0 +1,9 @@
+/Users/derekwarner/repos/rust-projects/command-line-utils/headr/target/debug/deps/libppv_lite86-9bf332c1fd2b4dcb.rmeta: /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/lib.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/soft.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/types.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/x86_64/mod.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/x86_64/sse2.rs
+
+/Users/derekwarner/repos/rust-projects/command-line-utils/headr/target/debug/deps/ppv_lite86-9bf332c1fd2b4dcb.d: /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/lib.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/soft.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/types.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/x86_64/mod.rs /Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/x86_64/sse2.rs
+
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/lib.rs:
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/soft.rs:
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/types.rs:
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/x86_64/mod.rs:
+/Users/derekwarner/.cargo/registry/src/index.crates.io-6f17d22bba15001f/ppv-lite86-0.2.17/src/x86_64/sse2.rs:
